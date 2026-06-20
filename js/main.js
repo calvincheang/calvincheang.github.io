59f80893a974bfd44fd4b94083d8c1c3957f36d4
calvincheang <cheang.calv011@gmail.com>
2026-06-20 00:23:17 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,3 +6,12 @@ button.onclick=()=>{
 menu.classList.toggle("show");
 
 }
+
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+
+    navLinks.classList.toggle("active");
+
+});
